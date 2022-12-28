e2e041419b397f43c7ad611bfaaf912252d2b32f
alonzoalcantar <pinoa.03@outlook.com>
2022-12-28 15:40:47 -0700

    Added app pages to navbar

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name= 'home'),
+    path('about/', views.about, name= 'about'),
+    path('businesses/', views.business_index, name= 'index'),
+    path('groupclasses/', views.groupclass_index, name= 'class_index'),
+    path('wellnesscoaches/',views.coach_index, name='coach_index'),
+
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-# Create your views here.
+def about(request):
+    return render(request, 'about.html')
+def business_index(request):
+    return render(request, 'business/index.html')
+
+def groupclass_index(request):
+    return render(request, '')
