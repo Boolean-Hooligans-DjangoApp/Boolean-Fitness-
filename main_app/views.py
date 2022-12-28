f536cb0f0a64c7595fdbef90d04a6a2fbdcfef7d
Joseph-King93 <j.king61693@gmail.com>
2022-12-28 17:36:52 -0500

    set up profile page/view

--- main_app/views.py
+++ main_app/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-# Create your views here.
+def about(request):
+    return render(request, 'about.html')
+
+def profile(request):
+    return render(request, 'profile/profile.html', { 'profile': profile })
+
