68ba5f7686688f1bf160f1078c865ea6ba50f67e
alonzoalcantar <pinoa.03@outlook.com>
2022-12-28 18:26:16 -0700

    created business model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    business_type = models.CharField(max_length=100)
+    business_hours = models.TextField(max_length=250)
+    rates = models.TextField(max_length=250)
