150ef4133ca412b483211697b57ede0942f7571f
alonzoalcantar <pinoa.03@outlook.com>
2023-01-05 14:03:47 -0700

    Seach bar can search through business model names

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.views.generic import ListView, FormView
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery
 from .forms import SignUpForm
 from .models import Business, GroupClass, Coach
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -66,8 +66,9 @@ class SearchResultsView(ListView, FormView):
     def get_queryset(self):
         query = self.request.GET.get('search')
         object_list = Business.objects.annotate(
-            search=SearchVector('name','location'),
-        ).filter(search=query)
+            search = SearchVector('name', 'location'),
+            ).filter(search=SearchQuery(query))
+        
         return object_list
 
 
